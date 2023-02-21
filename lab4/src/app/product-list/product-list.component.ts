f069752899b23e8697e1bdd674500907d28ff5f8
aalmat <madiyarbek.a2004@gmail.com>
2023-02-22 01:18:25 +0600

    getting started

--- lab4/src/app/product-list/product-list.component.ts
+++ lab4/src/app/product-list/product-list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { products } from '../products';
+import { products } from '../kaspi';
 
 @Component({
   selector: 'app-product-list',
@@ -8,16 +8,14 @@ import { products } from '../products';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+
   products = products;
 
-  share() {
-    window.alert('The product has been shared!');
+  share(link?:string) {
+    window.location.href = 'https://t.me/share/url?url='+link;
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
   }
 }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
